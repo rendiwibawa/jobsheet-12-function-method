443fbc69e430c39a2968de320be8c5c93d099c29
Rendi prayata wibawa <54298924+rendiwibawa@users.noreply.github.com>
2019-09-26 16:48:11 +0700

    Update Latihan3.java

--- Latihan3.java
+++ Latihan3.java
@@ -1,6 +1,6 @@
 package jobsheet_12_function_method;
 public class Latihan3 {
-    public static double a(double x,int y, int z) {
+    public static double a(int x,int y, int z) {
         return x+y+z;
     }
     public static void main(String[] args) {
